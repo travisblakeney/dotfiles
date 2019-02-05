f2a546d200ad15e0eee5851b779ec49ee8f28d4b
Travis Blakeney <tblakeney@l4170.seek.int>
2019-02-05 12:48:52 +1100

    execute setup-tools as a zsh script

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 defaults write com.apple.finder AppleShowAllFiles YES
 git clone --recursive git@github.com:travisblakeney/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
@@ -6,7 +6,6 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-chsh -s /bin/zsh
 
 sudo curl -L -o /usr/local/bin/amm https://git.io/vXVf5
 sudo chmod +x /usr/local/bin/amm
